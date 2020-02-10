1072040cb28e3ba85d1bd81789ec43ee37530ffe
CHOI EUNJOUNG <48176855+soleileun@users.noreply.github.com>
2020-02-10 11:59:30 +0900

    Solved

---
+++ 2020_FEB/BOJ_14581.java
@@ -0,0 +1,19 @@
+package day9;
+
+import java.util.Scanner;
+
+public class BOJ_14581 {
+
+	public static void main(String[] args) {
+
+
+		
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		
+		System.out.println(":fan::fan::fan:");
+		System.out.printf(":fan::%s::fan:\n",name);
+		System.out.println(":fan::fan::fan:");
+	}
+
+}
