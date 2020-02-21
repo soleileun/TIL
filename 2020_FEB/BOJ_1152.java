b7f67d663fe153f45c771b7daed4380be3f25dc4
CHOI EUNJOUNG <48176855+soleileun@users.noreply.github.com>
2020-02-21 23:59:00 +0900

    Solved

---
+++ 2020_FEB/BOJ_1152.java
@@ -0,0 +1,18 @@
+package hyoukang;
+
+import java.util.Scanner;
+
+public class BOJ_1152 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		
+		String splited[] = s.split(" ");
+		
+		System.out.println(splited.length);
+
+	}
+
+}
